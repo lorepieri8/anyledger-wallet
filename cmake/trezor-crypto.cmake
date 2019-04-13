0eeefd79f92701563b097c825082cffa11a14040
Lorenzo <lorepieri@hotmail.it>
2019-04-13 01:52:42 -0400

    hotfix

--- cmake/trezor-crypto.cmake
+++ cmake/trezor-crypto.cmake
@@ -4,18 +4,19 @@ include(ExternalProject)
 set(mylib_build_dir ${CMAKE_CURRENT_BINARY_DIR}/trezor-crypto)
 set(mylib_source_dir ${CMAKE_CURRENT_BINARY_DIR}/thirdparty/trezor-crypto)
 
+message(!!!!  ${CMAKE_CURRENT_BINARY_DIR}/trezor-crypto)
+message(!!!!   ${CMAKE_CURRENT_BINARY_DIR}/thirdparty/trezor-crypto)
+
 ExternalProject_Add(
   trezor-crypto-lib
 
   PREFIX     ${mylib_build_dir} # Root dir for entire project
   GIT_REPOSITORY "https://github.com/trezor/trezor-crypto.git"
   GIT_TAG "master"
+  SOURCE_DIR ${mylib_source_dir}
   UPDATE_COMMAND ""
   PATCH_COMMAND ${CMAKE_COMMAND} -E copy
   "${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/trezor-crypto/CMakeLists.txt" ${mylib_source_dir}/CMakeLists.txt
-  
-  SOURCE_DIR ${mylib_source_dir}
-  
   CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/install
              -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
              -DCMAKE_RANLIB=${CMAKE_RANLIB}
--- cmake/wallet.cmake
+++ cmake/wallet.cmake
@@ -1,6 +1,8 @@
 # recompile abi compiler targets if compiler source changes
 file(GLOB ABI_COMPILER_SOURCES "abi_compiler/*.py")
 
+
+
 link_directories(${CMAKE_CURRENT_BINARY_DIR}/install/lib/)
 include_directories(${CMAKE_CURRENT_BINARY_DIR}/install/include/)
 include_directories(${CMAKE_CURRENT_BINARY_DIR}/install/include/trezor-crypto)
@@ -8,6 +10,8 @@ include_directories(${CMAKE_CURRENT_SOURCE_DIR})
 add_subdirectory(eth)
 add_subdirectory(sawtooth)
 add_subdirectory(helpers)
+
+message(?????????)
 
 add_library(wallet STATIC wallet/wallet.c)
 set_target_properties(wallet PROPERTIES LINKER_LANGUAGE C)
